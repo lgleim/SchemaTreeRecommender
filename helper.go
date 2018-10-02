2d31359e68c3088240393515a998f707c77d89f0
Lars Gleim <gleim@cs.rwth-aachen.de>
2018-10-02 16:39:02 +0200

    more detailed memory statistics

--- helper.go
+++ helper.go
@@ -11,12 +11,12 @@ func PrintMemUsage() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("Alloc = %.2f MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %.2f MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %.2f MiB", bToMb(m.Sys))
 	fmt.Printf("\tNumGC = %v\n", m.NumGC)
 }
 
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
+func bToMb(b uint64) float64 {
+	return float64(b) / 1024 / 1024
 }
