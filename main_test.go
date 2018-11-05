0f235070ea15d52e5edd48dbdd3e4a20324afb5a
Lars Gleim <gleim@cs.rwth-aachen.de>
2018-11-05 12:00:45 +0100

    main test

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+// func TestTimeConsuming(t *testing.T) {
+//     if testing.Short() {
+//         t.Skip("skipping test in short mode.")
+//     }
+//     ...
+// }
+
+func TestTwoPass(t *testing.T) {
+	twoPass("10M.nt.gz", 100000)
+}
