19ccc9c7c33b40f1f281b5f2621a9457e36464bb
Lars Gleim <gleim@cs.rwth-aachen.de>
2018-11-13 11:40:40 +0100

    refactoring

--- main.go
+++ main.go
@@ -15,32 +15,42 @@ func twoPass(fileName string, firstN uint64) *SchemaTree {
 	// first pass: collect I-List and statistics
 	t1 := time.Now()
 
-	schema := SchemaTree{
-		propMap: make(propMap),
-		typeMap: make(typeMap),
-		Root:    newRootNode(),
-		MinSup:  3,
-	}
+	var schema SchemaTree
 
-	PrintMemUsage()
-
-	counter := func(s subjectSummary) {
-		for _, prop := range s.properties {
-			prop.increment()
+	if _, err := os.Stat(fileName + ".firstPass.bin"); os.IsNotExist(err) {
+		// preprocess
+		schema = SchemaTree{
+			propMap: make(propMap),
+			typeMap: make(typeMap),
+			Root:    newRootNode(),
+			MinSup:  3,
 		}
-	}
-	subjectSummaryReader(fileName, &schema.propMap, &schema.typeMap, counter, firstN)
 
-	fmt.Println("First Pass:", time.Since(t1))
-	PrintMemUsage()
+		counter := func(s *subjectSummary) {
+			for _, prop := range s.properties {
+				prop.increment()
+			}
+		}
+		subjectSummaryReader(fileName, &schema.propMap, &schema.typeMap, counter, firstN)
+
+		fmt.Println("First Pass:", time.Since(t1))
+		PrintMemUsage()
+		schema.Save(fileName + ".firstPass.bin")
+	} else {
+		tmp, err := LoadSchemaTree(fileName + ".firstPass.bin")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		schema = *tmp
+	}
 
 	// second pass
 	t1 = time.Now()
 
 	schema.updateSortOrder()
 
-	inserter := func(s subjectSummary) {
-		schema.Insert(&s, false)
+	inserter := func(s *subjectSummary) {
+		schema.Insert(s, false)
 	}
 	subjectSummaryReader(fileName, &schema.propMap, &schema.typeMap, inserter, firstN)
 
@@ -51,11 +61,14 @@ func twoPass(fileName string, firstN uint64) *SchemaTree {
 }
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	fileName := flag.String("file", "10M.nt.gz", "the file to parse")
 	firstNsubjects := flag.Int64("n", 0, "Only parse the first n subjects") // TODO: handle negative inputs
 	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to `file`")
 	memprofile := flag.String("memprofile", "", "write memory profile to `file`")
 	traceFile := flag.String("trace", "", "write execution trace to `file`")
+	loadBinary := flag.String("load", "", "read stored schematree from `file`")
 
 	// parse commandline arguments/flags
 	flag.Parse()
@@ -85,12 +98,27 @@ func main() {
 	}
 
 	t1 := time.Now()
-	schema := twoPass(*fileName, uint64(*firstNsubjects))
+	var schema *SchemaTree
 
-	// r := &renderer.PNGRenderer{
-	// 	OutputFile: "my_graph.png",
-	// }
-	// r.Render(fmt.Sprint(schema))
+	if *loadBinary != "" {
+		var err error
+		schema, err = LoadSchemaTree(*loadBinary)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		schema = twoPass(*fileName, uint64(*firstNsubjects))
+
+		// r := &renderer.PNGRenderer{
+		// 	OutputFile: "my_graph.png",
+		// }
+		// r.Render(fmt.Sprint(schema))
+
+		PrintMemUsage()
+
+		schema.Save("schemaTree.bin")
+
+	}
 
 	rdftype := schema.propMap.get("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 	memberOf := schema.propMap.get("http://www.wikidata.org/prop/direct/P463")
@@ -99,19 +127,10 @@ func main() {
 
 	t1 = time.Now()
 	rec := schema.recommendProperty(list)
+	fmt.Println(rec[:10])
 	fmt.Println(time.Since(t1))
 
 	PrintMemUsage()
-	fmt.Println(rec[:10])
-
-	schema.Save("schemaTree.bin")
-	schema, err := LoadSchemaTree("schemaTree.bin")
-	if err != nil {
-		fmt.Println(err)
-	}
-	rec = schema.recommendProperty(list)
-	PrintMemUsage()
-	fmt.Println(rec[:10])
 
 	if *memprofile != "" {
 		f, err := os.Create(*memprofile)
--- schemaTree.go
+++ schemaTree.go
@@ -110,7 +110,7 @@ func (tree *SchemaTree) Support(properties iList) uint32 {
 
 	// check all branches that include least frequent term
 	for term := properties[len(properties)-1].traversalPointer; term != nil; term = term.nextSameID {
-		if term.prefixContains(&properties) {
+		if term.prefixContains(properties) {
 			support += term.Support
 		}
 	}
@@ -119,7 +119,7 @@ func (tree *SchemaTree) Support(properties iList) uint32 {
 }
 
 func (tree *SchemaTree) recommendProperty(properties iList) propertyRecommendations {
-	var setSupport uint32
+	var setSupport uint64
 	//tree.root.support // empty set occured in all transactions
 
 	properties.sort() // descending by support
@@ -137,14 +137,17 @@ func (tree *SchemaTree) recommendProperty(properties iList) propertyRecommendati
 	}
 
 	// walk from each leaf towards root...l
-	for leaf := properties[len(properties)-1].traversalPointer; leaf != nil; leaf = leaf.nextSameID {
-		if leaf.prefixContains(&properties) {
-			setSupport += leaf.Support // number of occuences of this set of properties in the current branch
+	for leaf := properties[len(properties)-1].traversalPointer; leaf != nil; leaf = leaf.nextSameID { // iterate all instances for that property
+		if leaf.prefixContains(properties) {
+			setSupport += uint64(leaf.Support) // number of occuences of this set of properties in the current branch
+
+			// walk up
 			for cur := leaf; cur.parent != nil; cur = cur.parent {
 				if !(pSet[cur.ID]) {
 					candidates[cur.ID] += leaf.Support
 				}
 			}
+			// walk down
 			makeCandidates(leaf)
 		}
 	}
@@ -234,7 +237,7 @@ func (tree *SchemaTree) Save(filePath string) error {
 	// encode propMap
 	props := make([]*iItem, len(tree.propMap), len(tree.propMap))
 	for _, p := range tree.propMap {
-		props[p.sortOrder] = p
+		props[int(p.sortOrder)] = p
 	}
 	err = e.Encode(props)
 	if err != nil {
@@ -288,6 +291,7 @@ func LoadSchemaTree(filePath string) (*SchemaTree, error) {
 		fmt.Printf("Encountered error while trying to decompress the file: %v\n", err)
 		return nil, err
 	}
+	defer r.Close()
 
 	tree := new(SchemaTree)
 	// err = sereal.Unmarshal(serialized, tree)
@@ -304,6 +308,7 @@ func LoadSchemaTree(filePath string) (*SchemaTree, error) {
 		item.sortOrder = uint16(sortOrder)
 		tree.propMap[*item.Str] = item
 	}
+	fmt.Printf("%v properties... ", len(props))
 
 	// decode typeMap
 	var types map[uintptr]*iType
@@ -315,6 +320,7 @@ func LoadSchemaTree(filePath string) (*SchemaTree, error) {
 	for _, t := range types {
 		tree.typeMap[*t.Str] = t
 	}
+	fmt.Printf("%v types... ", len(types))
 
 	// decode MinSup
 	err = d.Decode(&tree.MinSup)
@@ -323,7 +329,8 @@ func LoadSchemaTree(filePath string) (*SchemaTree, error) {
 	}
 
 	// decode Root
-	err = tree.Root.decodeGob(d, &props, &types)
+	fmt.Printf("decoding tree...")
+	err = tree.Root.decodeGob(d, props, types)
 
 	if err != nil {
 		fmt.Printf("Encountered error while decoding the file: %v\n", err)
