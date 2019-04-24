bdf38cf8e9095b718270600c04002c738628f38d
Lars Gleim <lars.gleim@rwth-aachen.de>
2019-04-24 19:34:28 +0200

    Update temper-monkey-recommender-wikidata.js
    Former-commit-id: 19cb5e7ab017b5b288b043e29cf628c907d0cfff

--- scripots/temper-monkey-recommender-wikidata.js
+++ scripots/temper-monkey-recommender-wikidata.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         recommeender sematic web
-// @namespace    http://tampermonkey.net/
+// @name         SchemaTree Reccomender
+// @namespace    http://example.org/
 // @version      0.1
-// @description  try to take over the world!
-// @author       Daniel Hoppe Alvarez
+// @description  SchemaTree Wikidata Demo
+// @author       Lars Gleim, Daniel Hoppe Alvarez
 // @match        https://www.wikidata.org/wiki/*
-// @grant    GM_addStyle
-// @require http://code.jquery.com/jquery-3.3.1.min.js
-// @include 5.189.164.102:8081/recommender
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// @include      5.189.164.102:8081/recommender
 // @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
@@ -18,7 +18,7 @@
     const apiURL = "http://5.189.164.102:8081/recommender";
     const endpointUrl = 'https://query.wikidata.org/sparql'
     const wikidataURL = "http://www.wikidata.org/prop/direct/"
-    const w3sparql  = "https://lov.linkeddata.es/dataset/lov/sparql"
+    const w3sparql = "https://lov.linkeddata.es/dataset/lov/sparql"
 
     // whitelist
     const whitelist_url = "/prop/direct/"
@@ -31,15 +31,17 @@
     var formatted_list_of_statements = [];
     var display_statements = [];
 
+    // determine which properties currently exist
     for(var i = 0; i < amount_of_statements; i ++ ){
         // access a statement in list
-        var cur_statement   = all_document_statements[i];
+        var cur_statement = all_document_statements[i];
         var cur_property_id = cur_statement.id
 
         var link = wikidataURL + cur_property_id;
         formatted_list_of_statements.push(link)
     }
 
+    // get the list of recommendations from the SchemaTree Recommender
     GM_xmlhttpRequest ( {
       method:         "POST",
       url:            apiURL,
@@ -59,27 +61,29 @@
          return;
      }
 
+     var recommendations = rspObj.response;
+     console.log("Received recommendations from SchemaTree recommender:", recommendations)
+
      // go through all recommendations and get all
      // information needed to show them in front end
-     var recommendations = rspObj.response;
-       console.log(recommendations)
+
      for( var k = 0; k < recommendations.length; k++){
 
          // get recommendation
          var recommendation = recommendations[k];
          var recommendation_url = recommendations[k].Property.Str
-         var propability = recommendation.Probability
+         var probability = recommendation.Probability
          // check if is wikidata
          var isWikidata = recommendation_url.indexOf(whitelist_url) !== -1;
 
          if(isWikidata){
-             console.log("\n", recommendation_url, "\n",recommendation.Probability)
+//              console.log("\n", recommendation_url, "\n",recommendation.Probability)
              // get the infromation from the wikidata sparql endpoint
-             makeSPARQLQuery(endpointUrl,propability, createQueryStr(recommendation_url), function( data, probability ) {
+             makeSPARQLQuery(endpointUrl,probability, createQueryStr(recommendation_url), function( data, probability ) {
 
                  var bindings = data.results.bindings;
                  var info = {
-                     propability: probability,
+                     probability: probability,
                      label: bindings[0].propLabel.value,
                      uri: bindings[0].prop.value
                  }
@@ -91,11 +95,11 @@
 
               makeSPARQLQuery(w3sparql, createQueryLovStr( recommendation_url), function( data ) {
 
-                
+
                  var bindings = data.results.bindings;
                  if(bindings.length > 0){
                      var info = {
-                         propability: recommendation.Probability,
+                         probability: recommendation.Probability,
                          label: bindings[0].propLabel.value,
                          uri: recommendation_url
                      }
@@ -110,38 +114,53 @@
      }
    }
 
-console.log(display_statements)
+// console.log(display_statements)
     document.addEventListener('click', function (event) {
         // has to be the last step in the eventloop
         setTimeout(function(){
-            // checks if list with propositions is existing
+            // remove existing recommendations
+            document.querySelectorAll('ul.ui-ooMenu.ui-widget.ui-widget-content.ui-suggester-list.ui-entityselector-list li').forEach((x)=>x.remove())
+
+            // get the recommendation boxes
             var all_propositions = document.querySelectorAll('ul.ui-ooMenu.ui-widget.ui-widget-content.ui-suggester-list.ui-entityselector-list');
+            // Convert NodeList to JS Array
+            all_propositions = Array.prototype.slice.call(all_propositions);
+
             // set max heigt of ul list
-            $(all_propositions).css('max-height','250px');
-            if (all_propositions.length === 0){
-                return;
-            }
+            //$(all_propositions).css('max-height','250px');
 
-            // replace all existing recommendations
-            for (var i = 1; i < all_propositions.length; i++){
 
-                $(all_propositions[i]).children().each(function(index) {
+//             // replace all existing recommendations
+//             for (var i = 1; i < all_propositions.length; i++){
 
-                    var item = display_statements[index]
-                    var item_id = item.uri.replace(whitelist_url, "");
+//                 $(all_propositions[i]).children().each(function(index) {
 
-                    // override existing list recommendations with new ones
-                    $(this).replaceWith(generateHTMLlistItem(item_id, item));
-                })
-            }
+//                     var item = display_statements[index]
+//                     if (!!item) {
+//                         var item_id = item.uri.replace(whitelist_url, "");
 
-            all_propositions = sortByKey(all_propositions, 'Probability')
-            // append all recommendations that are not replaced
-            for (var j = all_propositions.length -1; j < display_statements.length; j++){
+//                         // override existing list recommendations with new ones
+//                         $(this).replaceWith(generateHTMLlistItem(item_id, item));
+//                     }
+//                 })
+//             }
+
+            // sort recommendations by probability
+            display_statements.sort(function(a,b){ // c.f. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Description
+                let x = b.probability - a.probability;
+                if (x === 0) { // otherwise sort alphabetically
+                    return a.label.toLowerCase() < b.label.toLowerCase() ? -1 : 1;
+                }
+                return x;
+            })
+
+            // render recommendations
+            for (var j = 0; j < display_statements.length; j++){
                 var item = display_statements[j]
                 var item_id = item.uri.replace(whitelist_url, "");
                 $(all_propositions).append(generateHTMLlistItem(item_id, item))
             }
+            //all_propositions = sortByKey(all_propositions, 'Probability')
 
         }, 0);
 
@@ -194,7 +213,7 @@ console.log(display_statements)
             '<a tabindex="-1" href="//www.wikidata.org/wiki/Property:' + item_id + '">' +
             '<span class="ui-entityselector-itemcontent">'+
             '<span class="ui-entityselector-label">'+ item.label +'</span>'+
-            '<span class="ui-entityselector-description">' + 'The probability this entity should have this attribute is: '+ item.propability+'</span>' + '</span>' +
+            '<span class="ui-entityselector-description">The probability this entity should have this attribute is: '+ item.probability+'</span></span>' +
             '</a>' +
             '</li> '
     }
