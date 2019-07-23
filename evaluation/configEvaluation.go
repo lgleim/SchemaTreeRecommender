70b0a86e25f7af0d58d2a34386a44e434e4bff25
Maximilian Peters <maximilian.peters1@rwth-aachen.de>
2019-07-23 12:02:42 +0200

    variance -> stddev
    Former-commit-id: f5b91659f3e49e6c2a1498731ed12ef54584f5bf

--- evaluation/configEvaluation.go
+++ evaluation/configEvaluation.go
@@ -43,10 +43,10 @@ func runConfig(name *string, tree *schematree.SchemaTree, typed bool, handler st
 }
 
 func writeCSV(evaluation *[]evalSummary, filename string) {
-	output := fmt.Sprintf("%8v, %8v, %8v, %8v, %12v, %8v, %8v, %8v, %8v, %10v, %10v, %8v, %8v, %8v, %8v, %8v\n", "Config No.", "set", "median", "mean", "variance", "top1", "top5", "top10", "worst5avg", "sampleSize", "#subjects", "duration", "hitRate", "Precision", "Precision At 10", "Recommendation Count")
+	output := fmt.Sprintf("%8v, %8v, %8v, %8v, %12v, %8v, %8v, %8v, %8v, %10v, %10v, %8v, %8v, %8v, %8v, %8v\n", "Config No.", "set", "median", "mean", "stddev", "top1", "top5", "top10", "worst5avg", "sampleSize", "#subjects", "duration", "hitRate", "Precision", "Precision At 10", "Recommendation Count")
 	e := *evaluation
 	for i, eval := range e {
-		output += fmt.Sprintf("%8v, %8v, %8v, %8v, %12v, %8v, %8v, %8v, %8v, %10v, %10v, %8v,%8v, %8v,%8v, %8v \n", i, eval.setSize, eval.median, eval.mean, eval.variance, eval.top1, eval.top5, eval.top10, eval.worst5average, eval.sampleSize, eval.subjectCount, eval.duration, eval.hitRate, eval.precision, eval.precisionAt10, eval.recommendationCount)
+		output += fmt.Sprintf("%8v, %8v, %8v, %8v, %12v, %8v, %8v, %8v, %8v, %10v, %10v, %8v,%8v, %8v,%8v, %8v \n", i, eval.setSize, eval.median, eval.mean, eval.stddev, eval.top1, eval.top5, eval.top10, eval.worst5average, eval.sampleSize, eval.subjectCount, eval.duration, eval.hitRate, eval.precision, eval.precisionAt10, eval.recommendationCount)
 	}
 	f, _ := os.Create(filename)
 	f.WriteString(output)
